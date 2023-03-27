e6f0f3de0815dd5df7efbbf0bbf420eb3dc4f1ec
KobyD <dkdk100@walla.co.il>
2023-03-27 16:43:57 +0300

    add model page methods

--- xyte/LawnMowers/Pages/ModelPage.py
+++ xyte/LawnMowers/Pages/ModelPage.py
@@ -1,7 +1,16 @@
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
+from xyte.LawnMowers.Pages.locators import ModelPageLocators
+
 
 class ModelPage(object):
 
-    pass
+    def __init__(self, driver):
+        self.driver = driver
+
+
+
+    def click_on_add_model(self):
+        print('Try to click on add model')
+        add_mode = self.driver.find_element(*ModelPageLocators.ADD_MODEL)
--- xyte/LawnMowers/Pages/locators.py
+++ xyte/LawnMowers/Pages/locators.py
@@ -10,7 +10,10 @@ class WelcomePageLocators(object):
 class LoginPageLocators(object):
     USER = (By.ID, 'mantine-hlflckkm2')
     PW = (By.ID, 'mantine-zma8mduxv')
-    LOGIN = (By.CSS_SELECTOR,'span[class*=mantine-14wyo4q]')
+    LOGIN = (By.CSS_SELECTOR,'span[class*="mantine-14wyo4q"]')
 
 class WelcomePageLocators(object):
     MODElS_SIDE_BAR = (By.HREF, '/models')
+
+class ModelPageLocators(object):
+    ADD_MODEL = (By.CSS_SELECTOR, 'span[class*="mantine-1t9xh9a"]')
