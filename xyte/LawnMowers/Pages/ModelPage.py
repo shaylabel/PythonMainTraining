6fce7c68c956035a94ec640a6e926b2241f14296
KobyD <dkdk100@walla.co.il>
2023-03-27 17:24:00 +0300

    add save model

--- xyte/LawnMowers/Pages/ModelPage.py
+++ xyte/LawnMowers/Pages/ModelPage.py
@@ -14,8 +14,16 @@ class ModelPage(object):
         add_model = self.driver.find_element(*ModelPageLocators.ADD_MODEL)
         add_model.click()
 
-    def fill_model(self,model_name):
-        print('Try to fill model page')
+    def fill_model(self,name_pattern):
+        print('Try to fill model page ')
+        model_name = self.driver.find_element(*ModelPageLocators.MODEL_NAME)
+        model_name.click()
+        model_name.clear()
+        model_name.send_keys(name_pattern)
+        save_model = self.driver.find_element(*ModelPageLocators.SAVE_MODEL) # to do - add is clickable before click
+        save_model.click()
+
+
 
     def delete_model(self,model_name):
          print('Try to delete model ')
--- xyte/LawnMowers/Pages/locators.py
+++ xyte/LawnMowers/Pages/locators.py
@@ -17,3 +17,5 @@ class WelcomePageLocators(object):
 
 class ModelPageLocators(object):
     ADD_MODEL = (By.CSS_SELECTOR, 'span[class*="mantine-1t9xh9a"]')
+    MODEL_NAME = (By.ID,'mantine-fyukqfg4a')
+    SAVE_MODEL = (By.CSS_SELECTOR,'button[class*="mantine-UnstyledButton-root"]')
