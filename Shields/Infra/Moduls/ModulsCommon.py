f070ac3c49e780bc508be38a35f5aa08f5d82a15
KobyD <dkdk100@walla.co.il>
2023-04-04 11:02:01 +0300

    fix at module common

--- Shields/Infra/Moduls/ModulsCommon.py
+++ Shields/Infra/Moduls/ModulsCommon.py
@@ -8,3 +8,6 @@ def repeat_same_action():
 
 def manage_not_exist_module():   # try to manage not exist module without flow (e.g multimedia , navigation , diesel engine ....)
     pass
+
+def enable_module (Moduls):
+    pass
